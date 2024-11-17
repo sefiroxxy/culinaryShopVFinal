6951cd23ca3c1030dd5c2ef05de6fcee30a8cd4f
sefiroxxy <59780367+sefiroxxy@users.noreply.github.com>
2024-11-16 22:08:09 -0300

    Se usara ´ES7 + React/Redux/React-Native snippets´ para acelerar el desarrollo al proporcionar snippets o fragmentos de codigos ya hechos simples que pueden ser reutilizados

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="text-blue-600 ">
-           
-    </div>
+    <>
+      <Outlet/>
+    </>
   );
 }
 
